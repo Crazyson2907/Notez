6e29cb7ffb43df421dabbe43cba0d87318a6c1fb
Vlad <lubinvlad@icloud.com>
2022-05-16 23:31:22 +0300

    mess

--- app/src/main/java/crazyson/com/ua/notez/MainActivity.kt
+++ app/src/main/java/crazyson/com/ua/notez/MainActivity.kt
@@ -1,11 +1,8 @@
 package crazyson.com.ua.notez
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity {
+    fun onCreate(savedInstanceState: Bundle?) {
     }
 }
