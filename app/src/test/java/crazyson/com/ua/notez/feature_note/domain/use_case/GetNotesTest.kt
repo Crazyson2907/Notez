31e8d92f9bb3dfd22787f72a3133ab15a5cc5960
Vlad <lubinvlad@icloud.com>
2022-06-21 18:13:08 +0300

    test initial

---
+++ app/src/test/java/crazyson/com/ua/notez/feature_note/domain/use_case/GetNotesTest.kt
@@ -0,0 +1,47 @@
+package crazyson.com.ua.notez.feature_note.domain.use_case
+
+import com.google.common.truth.Truth.assertThat
+import crazyson.com.ua.notez.feature_note.data.repository.FakeNoteRepository
+import crazyson.com.ua.notez.feature_note.domain.model.Note
+import crazyson.com.ua.notez.feature_note.domain.util.NoteOrder
+import crazyson.com.ua.notez.feature_note.domain.util.OrderType
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+class GetNotesTest
+
+private lateinit var getNotes: GetNotes
+private lateinit var fakeRepository: FakeNoteRepository
+
+@Before
+fun setUp() {
+    fakeRepository = FakeNoteRepository()
+    getNotes = GetNotes(fakeRepository)
+
+    val notesToInsert = mutableListOf<Note>()
+    ('a'..'z').forEachIndexed { index, c ->
+        notesToInsert.add(
+            Note(
+                title = c.toString(),
+                content = c.toString(),
+                timeStamp = index.toLong(),
+                color = index
+            )
+        )
+    }
+    notesToInsert.shuffle()
+    runBlocking {
+        notesToInsert.forEach { fakeRepository.insertNote(it) }
+    }
+}
+
+    @Test
+    fun `Order notes by title ascending, correct order` () = runBlocking {
+        val notes = getNotes(NoteOrder.Title(OrderType.Ascending)).first()
+
+        for(i in 0..notes.size - 2) {
+            assertThat(notes[i].title).isLessThan(notes[i+1].title)
+    }
+}
